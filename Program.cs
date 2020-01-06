204ce353afe0930ea5cfece0d93f85ef8ac61383
kyle <kcvaughn75@gmail.com>
2020-01-06 08:36:19 -0500

    All files for deliverable2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine().ToUpper();
+
+            int checksum = 0;
+            string message = "";
+
+            foreach (char c in input)
+            {
+                int code = (int)c;
+                checksum += code;
+                code = code - 64;
+                message += (code.ToString()) + "-";
+            }
+
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+        }
+    }
+}
